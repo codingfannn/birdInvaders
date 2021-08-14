bf34c44bbd43fa325bd59f1b1b500956ffc90690
Noemi Radvan <radvanno@gmail.com>
2021-08-14 09:20:00 +0200

    growing birds

--- obstacle.js
+++ obstacle.js
@@ -1,17 +1,19 @@
 class Obstacle {
   constructor() {
-    this.width = 100;
-    this.height = 70;
+    this.width = 50;
+    this.height = 35;
     //this.x = 1500;
     this.x = CANVAS_WIDTH + this.width;
-    this.y = 150;
-    this.speed = 10;
+    this.y = 300;
+    this.speed = 5;
     this.currentBird = random(birds);
   }
 
   draw() {
-    this.x -= 8;
-    this.y += 2;
+    this.x -= 7;
+    //this.y +=
+    this.height += random(0.3, 0.5);
+    this.width += random(0.1, 0.3);
     image(this.currentBird, this.x, this.y, this.width, this.height);
     //if (this.x <= -this.width) {
     //  this.x = 0;
